a8e7102b129b2f29b97462d04b6667f98abc6089
VanHoang-IT <0335823877vh@gmail.com>
2026-04-06 20:21:58 +0700

    update test_auth

--- cardapp/dao/user_dao.py
+++ cardapp/dao/user_dao.py
@@ -14,19 +14,32 @@ def auth_user(username, password):
     return User.query.filter(User.username == username,
                              User.password == password).first()
 
+
 def add_user(name, username, password, avatar, email):
+    if not name:
+        raise ValueError("Thiếu trường tên")
+    if not email:
+        raise ValueError("Thiếu trường email")
+
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        raise ValueError("Email không hợp lệ")
+
     if len(username) < 5:
-        raise ValueError("username phai it nhat co 5 ki tu")
+        raise ValueError("Username phải ít nhất có 5 kí tự")
+
     if len(password) < 8:
-        raise ValueError("mat khau phai it nhat co 8 ki tu")
+        raise ValueError("Mật khẩu phải có ít nhất 8 kí tự")
+
     if not re.search(r'[0-9]', password):
         raise ValueError("Mật khẩu phải chứa ít nhất một chữ số")
     if not re.search(r'[a-z]', password):
         raise ValueError("Mật khẩu phải chứa ít nhất một chữ thường")
     if not re.search(r'[A-Z]', password):
         raise ValueError("Mật khẩu phải chứa ít nhất một chữ hoa")
+
     password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
     u = User(name=name.strip(), username=username.strip(), password=password, email=email)
+
     if avatar:
         res = cloudinary.uploader.upload(avatar)
         u.avatar = res.get("secure_url")
