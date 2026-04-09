ca0363810b1c2daadc2dda56ba9ed031822cadf1
donguyen6c <donguyen6c@gmail.com>
2026-04-09 18:06:56 +0700

    date filter to transactions page

--- cardapp/apis/history_api.py
+++ cardapp/apis/history_api.py
@@ -9,13 +9,23 @@ from cardapp import dao
 
 history_bp = Blueprint('history', __name__)
 
+
 @history_bp.route('/users/current-user/transactions', methods=['GET'])
 @login_required
 @swag_from('../docs/history.yml')
 def history_view():
     if not current_user.is_authenticated:
         return redirect('/login?next=/history')
-    page = request.args.get('page', 1, type=int)
-    receipts = dao.get_receipts_by_user(current_user.id, page=page)
 
-    return render_template('history.html', receipts=receipts)
+    from_date = request.args.get('from_date')
+    to_date = request.args.get('to_date')
+    page = request.args.get('page', 1, type=int)
+
+    receipts = dao.get_receipts_by_user(
+        user_id=current_user.id,
+        from_date=from_date,
+        to_date=to_date,
+        page=page
+    )
+
+    return render_template('history.html', receipts=receipts, from_date=from_date, to_date=to_date)
--- cardapp/dao/receipt_dao.py
+++ cardapp/dao/receipt_dao.py
@@ -3,8 +3,16 @@ from cardapp import db, utils
 from datetime import datetime
 from cardapp.utils import TRANSACTIONS_PAGE_SIZE
 
-def get_receipts_by_user(user_id, page = 1, per_page = TRANSACTIONS_PAGE_SIZE):
-    return Receipt.query.filter(Receipt.user_id == user_id).order_by(Receipt.created_date.desc()).paginate(page=page, per_page = per_page, error_out = False)
+def get_receipts_by_user(user_id, from_date=None, to_date=None, page=1, per_page=TRANSACTIONS_PAGE_SIZE):
+    query = Receipt.query.filter(Receipt.user_id == user_id)
+
+    if from_date:
+        query = query.filter(Receipt.created_date >= from_date)
+
+    if to_date:
+        query = query.filter(Receipt.created_date <= f"{to_date} 23:59:59")
+
+    return query.order_by(Receipt.created_date.desc()).paginate(page=page, per_page=per_page, error_out=False)
 
 def add_receipt(user_id, cart, discount_code=None):
     pass
