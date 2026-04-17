a2c6465f29f558dcb3fe7ca002cf0465faa3e175
donguyen6c <donguyen6c@gmail.com>
2026-04-17 16:04:26 +0700

    complete admin stats

--- cardapp/dao/stats_dao.py
+++ cardapp/dao/stats_dao.py
@@ -3,7 +3,11 @@ from cardapp import db
 from sqlalchemy import func
 
 def revenue_by_product():
-    pass
+    return (db.session.query(Product.id, Product.name, func.sum(ReceiptDetails.unit_price * ReceiptDetails.quantity))
+            .join(ReceiptDetails, ReceiptDetails.product_id == Product.id)
+            .group_by(Product.id, Product.name).all())
 
 def revenue_by_time(period="month"):
-    pass
+    return ((db.session.query(func.extract('month', Receipt.created_date), func.sum(Receipt.final_amount))
+            .group_by(func.extract('month', Receipt.created_date)))
+            .order_by(func.extract('month', Receipt.created_date)).all())
